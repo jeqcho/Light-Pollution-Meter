60a81a5419b695703f754b324d39322167d90b81
Chooi Je Qin <42904912+jeqinchooi@users.noreply.github.com>
2021-10-18 14:52:39 +0800

    Updated about

--- app/src/main/java/com/chojeq/lightpollutionmeter/AboutFragment.kt
+++ app/src/main/java/com/chojeq/lightpollutionmeter/AboutFragment.kt
@@ -1,19 +1,16 @@
 package com.chojeq.lightpollutionmeter
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.chojeq.lightpollutionmeter.R
 
 class AboutFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +21,8 @@ class AboutFragment : Fragment() {
         binding.findViewById<Button>(R.id.back_home_button).setOnClickListener{
             it.findNavController().navigate(R.id.action_aboutFragment_to_homeFragment)
         }
+        binding.findViewById<TextView>(R.id.github_link).movementMethod = LinkMovementMethod.getInstance()
+        binding.findViewById<TextView>(R.id.paper_link).movementMethod = LinkMovementMethod.getInstance()
         return binding
     }
 }
